8a76cec29c9a895e37f1f4aa7f85a11d1f5556f2
arhariri <ali.71hariri@gmail.com>
2017-07-16 16:27:51 +0430

    Modify getByShareLink query to be same in select clause as getByEmail query Filter hash used for share link (Just alphabetic characters and numbers are accepted)

--- sql/khatms/getByShareLink.sql
+++ sql/khatms/getByShareLink.sql
@@ -1,3 +1,21 @@
-select *
-from khatms
-where share_link = ${share_link}
+select
+  khatms.khid as khid,
+  khatms.name as khatm_name,
+  khatms.description,
+  khatms.creator_shown,
+  khatms.start_date,
+  khatms.end_date,
+  khatms.timezone,
+  khatms.specific_sura,
+  khatms.repeats,
+  users.name as owner_name,
+  users.email as owner_nmail,
+  khatms.share_link,
+  count(case when commitments.uid is not null then 1 end)  as commitment_pages,
+  count(case when (commitments.uid is not null and commitments.isread = true) then 1 end) as read_pages,
+  count(distinct commitments.uid) as participatings
+from commitments
+join khatms on commitments.khid = khatms.khid
+join users on users.uid = khatms.creator_id
+where khatms.share_link = ${share_link}
+group by khatms.khid, users.email, users.name

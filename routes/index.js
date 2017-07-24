8f4cd398d039704b33b57bd37e9cb34e3ef18201
arhariri <ali.71hariri@gmail.com>
2017-07-24 13:22:06 +0430

    Remove checking for security (Https)

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@ const path = require('path');
 /* Diverting unknown routes to Angular router */
 router.all("*",function(req,res,next){
   /* Redirect http to https */
-  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-    res.redirect('https://'+req.headers.host+req.url);
-  else if(req.originalUrl.indexOf('api') === -1) {
+  if(req.originalUrl.indexOf('api') === -1) {
     console.log('[TRACE] Server 404 request: ' + req.originalUrl);
     var p = path.join(__dirname, '../public', 'index.html').replace(/\/routes\//, '/');
     res.status(200).sendFile(p);

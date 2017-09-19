d100aeb73ddde893b547c8c8c0e9575d2492b932
arhariri <ali.71hariri@gmail.com>
2017-09-19 11:55:03 +0430

    Improve push notifications at 18:00 based on creator's timezone Send push notifications if one of two conditions is correct (today is end date of khatm or 0.8 RP/RD > readPages)

--- sql/khatms/getNotRead.sql
+++ sql/khatms/getNotRead.sql
@@ -5,9 +5,10 @@ select
     users.uid as uid,
     users.email as user_email,
     users.name as user_name,
-    count(commitments.uid) as remaining_pages
+    count(case when commitments.isread = false then 1 end) as remaining_pages,
+    count(case when commitments.isread = true then 1 end) as read_pages
 from khatms
 join commitments on khatms.khid = commitments.khid
 join users on commitments.uid = users.uid
-where commitments.uid is not null and commitments.isread = false
+where commitments.uid is not null and lower(khatms.timezone) = lower(${timezone})
 group by khatms.khid, users.uid, commitments.uid
